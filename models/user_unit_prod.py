443c2f34c89a84bb210ec9cad1b00a2df817bc53
aminearafy <aminearafy@gmail.com>
2022-04-19 13:46:59 +0000

    Annuler le chmps unit prod

--- models/user_unit_prod.py
+++ models/user_unit_prod.py
@@ -6,4 +6,4 @@ from odoo import models, fields
 class ProdIndividual(models.Model):
     _inherits = 'res.users'
 
-    up_user = fields.Boolean(string="Cet utilisateur est une unité de production")
+    #up_user = fields.Boolean(string="Cet utilisateur est une unité de production")
